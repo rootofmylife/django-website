bab5e8a6c4be3168be914bdbff0bd3ae79e7bfb5
Duc Do <dotrananhduc@gmail.com>
2018-02-26 14:33:12 +0700

    update views

--- mideas/views.py
+++ mideas/views.py
@@ -1,68 +1,59 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
 from django.shortcuts import render
 from django.http import Http404, HttpResponse
-from django.shortcuts import render
-from .models import FileUpload, UploadForm
 from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse
 import csv
 from django.http import StreamingHttpResponse
-
-class Echo:
-    """An object that implements just the write method of the file-like
-    interface.
-    """
-    def write(self, value):
-        """Write the value by returning it, instead of storing in a buffer."""
-        return value
-
+import nltk
+import codecs
 
 def home(request):
-    if request.method == "POST":
-
-        #for f in request.FILES.getlist('files'):
-        #    filename = f.name
-        #    listname.append(filename)
-        #file = request.FILES.getlist('files')
-        
+    if request.method == "POST":      
         for f in request.FILES.getlist('files'):
             if f.name == 'key.txt':
-                key = f.read().splitlines()
+                keys = f.read().splitlines()
             elif f.name == 'mess.txt':
                 mess = f.read().splitlines()
-        
 
-        key = [s.decode() for s in key]
+        keys = [s.decode() for s in keys]
         mess = [s.decode() for s in mess]
-        
-
-
-        del key[0]
+	
+	
+        del keys[0]
         del mess[0]
         rs = []
+        sym = r"~`!@#$%^&*()_-+=[]{}|;':\"”“,./<>?"
+        keys = [s.translate({ord(c): "" for c in sym}) for s in keys]
+        mess = [s.translate({ord(c): "" for c in sym}) for s in mess]
+        mess = [s.strip() for s in mess if s != 'NULL']
+        mess = [str(s) for s in mess]
 
-        rs.append(key)
-        rs.append(mess)
-        
-        '''
-        pseudo_buffer = Echo()
-        writer = csv.writer(pseudo_buffer)
-        response = StreamingHttpResponse((writer.writerow(r.decode('utf-8')) for r in rs),
-                                     content_type="text/csv")
-        response['Content-Disposition'] = 'attachment; filename="output.csv"'
-        '''
-        
+        keys = ' '.join(keys)
+        keys = keys.split()
+
+        mess = ' '.join(mess)
+        mess = list(set(mess.split()))
+        for i in keys:
+                temp = []
+                temp.append(i)
+                for j in mess:
+                        if i in j:
+                                temp.append(j)
+                        temp_dv = nltk.edit_distance(i.lower(), j.lower())
+                        if temp_dv < (len(i) - (len(i) * 0.5)) and temp_dv > 0:
+                                temp.append(j)
+                rs.append(temp)
         response = HttpResponse(content_type='text/csv')
         response['Content-Disposition'] = 'attachment; filename="somefilename.csv"'
-
+        response.write(codecs.BOM_UTF8)
         writer = csv.writer(response)
 
         for i in rs:
             writer.writerow(i)
             writer.writerow('\n')
-
-
+	
         return response
-        
-        #return HttpResponse(str(rs[0]))
 
     return render(request,'mideas/home.html')
